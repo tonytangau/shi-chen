fdf5ef8db86536c549d4ede85dc197ca8e27e172
Tony Tang <tony.cn@outlook.com>
2018-09-07 17:13:09 +0800

    Add dist

---
+++ dist/precache-manifest.084e981ea690108a51b2afd17daee782.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "9ecec62af737acf020a7",
+    "url": "/js/app.821ac7ec.js"
+  },
+  {
+    "revision": "f42aa9a9cdd556163cc73b3c90f1d042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ecec62af737acf020a7",
+    "url": "/css/app.59ee006f.css"
+  }
+];
